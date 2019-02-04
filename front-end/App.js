2bca41ce1675ac08f39fce8e01af312ae44a9e08
Christopher Sacré <csacre@wemanity.com>
2019-02-04 23:25:15 +0100

    FRONT-END | ADD QR CODE SCANNER

--- front-end/App.js
+++ front-end/App.js
@@ -8,23 +8,15 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet, Linking, View} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <QRCodeScanner onRead={(e) => Linking.openURL(e.data).catch(err => console.error('An error occured', err))}/>
       </View>
     );
   }
