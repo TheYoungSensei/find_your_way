16190459aced30d6b80b647fee5e103ced4bc511
Christopher Sacré <csacre@wemanity.com>
2019-02-13 22:56:13 +0100

    FRONT-END | Fix Fatal BUG

--- FindYourWayApp/index.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import Router from './router';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Router);
