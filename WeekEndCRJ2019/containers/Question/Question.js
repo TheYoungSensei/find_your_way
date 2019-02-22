16ecd60cb90cd7c51ab5377ceed66497e596b6aa
Christopher Sacré <csacre@wemanity.com>
2019-02-22 22:37:32 +0100

    FRONT-END | Add some style to question page

--- WeekEndCRJ2019/containers/Question/Question.js
+++ WeekEndCRJ2019/containers/Question/Question.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Text, Button } from 'react-native-elements';
 
 type Props = {
   navigation: any,
   questions: any,
 }
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  question: {
+    textAlign: 'center',
+  },
+  answer: {
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  buttonYes: {
+    backgroundColor: '#00FF00',
+    margin: 30,
+    width: 120,
+    padding: 10,
+  },
+  buttonNo: {
+    backgroundColor: '#FF0000',
+    margin: 30,
+    width: 120,
+    padding: 10,
+  },
+  title: {
+    fontSize: 22,
+  },
+};
 
 export default class Question extends Component<Props> {
   render() {
@@ -12,10 +44,24 @@ export default class Question extends Component<Props> {
     const id = navigation.getParam('questionId', -1);
     const question = questions[id];
     return (
-      <View>
-        <Text>
+      <View style={styles.container}>
+        <Text h3 style={styles.question}>
           {question}
         </Text>
+        <View style={styles.answer}>
+          <Button
+            buttonStyle={styles.buttonYes}
+            titleStyle={styles.title}
+            type="solid"
+            title="Oui"
+          />
+          <Button
+            buttonStyle={styles.buttonNo}
+            titleStyle={styles.title}
+            type="solid"
+            title="Non"
+          />
+        </View>
       </View>
     );
   }
