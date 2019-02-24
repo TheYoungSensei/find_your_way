947186d555980cd8eb1bfa5f260f3dde8172ba58
Christopher Sacré <csacre@wemanity.com>
2019-02-24 17:10:59 +0100

    BACK-END | QUESTIONS | Better winston logger

--- back-end/questions/src/utils/logger.js
+++ back-end/questions/src/utils/logger.js
@@ -1,9 +1,35 @@
 import winston from 'winston';
 
+const config = {
+  levels: {
+    error: 0,
+    debug: 1,
+    warn: 2,
+    data: 3,
+    info: 4,
+    verbose: 5,
+    silly: 6,
+    custom: 7,
+  },
+  colors: {
+    error: 'red',
+    debug: 'blue',
+    warn: 'yellow',
+    data: 'grey',
+    info: 'green',
+    verbose: 'cyan',
+    silly: 'magenta',
+    custom: 'yellow',
+  },
+};
+
+winston.addColors(config.colors);
+
 export const logger = winston.createLogger({
+  levels: config.levels,
   transports: [new winston.transports.Console()],
   format: winston.format.combine(
-    winston.format.colorize({ all: true }),
+    winston.format.colorize(),
     winston.format.timestamp('YYYY-MM-DD HH:mm:ss'),
     winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
   ),
